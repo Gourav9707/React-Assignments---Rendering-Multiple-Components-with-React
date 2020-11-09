4fb0213105268e5aa2cb8bdff9dbc12be340b37d
Gourav9707 <67501980+Gourav9707@users.noreply.github.com>
2020-11-09 18:01:17 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <div data-ns-test="project-name">E-commerce</div>
+      <div data-ns-test="project-description">Old Books buy and sell</div>
+    </>
+  );
+};
 
 export default App;
